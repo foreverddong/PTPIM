1893f58f717db396d3b483c562755a67f692b58d
foreverddong <foreverddong@icloud.com>
2015-10-11 17:40:25 +0800

    Create the project

---
+++ PTPIM/ConnetcionForm.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PTPIM
+{
+    partial class ConnetcionForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logBox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // logBox
+            // 
+            this.logBox.FormattingEnabled = true;
+            this.logBox.ItemHeight = 15;
+            this.logBox.Location = new System.Drawing.Point(12, 12);
+            this.logBox.Name = "logBox";
+            this.logBox.Size = new System.Drawing.Size(400, 124);
+            this.logBox.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 213);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(400, 25);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(164, 184);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Target IP";
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(98, 328);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(214, 62);
+            this.connectButton.TabIndex = 3;
+            this.connectButton.Text = "Connect!";
+            this.connectButton.UseVisualStyleBackColor = true;
+            // 
+            // ConnetcionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 430);
+            this.Controls.Add(this.connectButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.logBox);
+            this.Name = "ConnetcionForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox logBox;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button connectButton;
+    }
+}
+
